8b6659e706b9e5d0aa4beda6568d85f2fbec561c
omargawdat <omargawdaat@gmail.com>
2024-04-08 17:44:18 +0200

    fix question2 and question3

--- src/main/java/org/example/Q1_3/DifferenceMapper.java
+++ src/main/java/org/example/Q1_3/DifferenceMapper.java
@@ -7,23 +7,17 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 public class DifferenceMapper extends Mapper<LongWritable, Text, Text, Text> {
     private Text outputKey = new Text();
-
     private Text outputValue = new Text();
 
-    public void map(LongWritable key, Text value, Mapper<LongWritable, Text, Text, Text>.Context context) throws IOException, InterruptedException {
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String[] fields = value.toString().split(",");
         String tKey = fields[0];
-        Integer attributeValue = Integer.valueOf(0);
-        if (tKey.equals("T1")) {
-            attributeValue = Integer.valueOf(Integer.parseInt(fields[1].trim())); // Added trim() here
-            this.outputKey.set(tKey);
-            this.outputValue.set(attributeValue.toString());
-            context.write(this.outputKey, this.outputValue);
-        } else if (tKey.equals("T2")) {
-            attributeValue = Integer.valueOf(Integer.parseInt(fields[2].trim())); // And here
-            this.outputKey.set(tKey);
-            this.outputValue.set(attributeValue.toString());
-            context.write(this.outputKey, this.outputValue);
+        if ("T1".equals(tKey) || "T2".equals(tKey)) {
+            int fieldIndex = "T1".equals(tKey) ? 1 : 2;
+            Integer attributeValue = Integer.parseInt(fields[fieldIndex].trim());
+            outputKey.set(tKey);
+            outputValue.set(attributeValue.toString());
+            context.write(outputKey, outputValue);
         }
     }
 }
--- src/main/java/org/example/Q1_3/DifferenceReducer.java
+++ src/main/java/org/example/Q1_3/DifferenceReducer.java
@@ -8,31 +8,23 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 public class DifferenceReducer extends Reducer<Text, Text, Text, Text> {
-    List<Integer> t1Values = new ArrayList<>();
+    private List<Integer> t1Values = new ArrayList<>();
+    private List<Integer> t2Values = new ArrayList<>();
 
-    List<Integer> t2Values = new ArrayList<>();
-
-    public void reduce(Text key, Iterable<Text> values, Reducer<Text, Text, Text, Text>.Context context) throws IOException, InterruptedException {
-        String firstKey = "T1";
-        for (Text value : values) {
-            if (key.toString().equals("T1")) {
-                this.t1Values.add(Integer.valueOf(Integer.parseInt(value.toString())));
-                Collections.sort(this.t1Values);
-                continue;
+    @Override
+    public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        if ("T1".equals(key.toString())) {
+            for (Text value : values) {
+                t1Values.add(Integer.parseInt(value.toString()));
             }
-            if (key.toString().equals("T2")) {
-                this.t2Values.add(Integer.valueOf(Integer.parseInt(value.toString())));
-                Collections.sort(this.t2Values);
+        } else if ("T2".equals(key.toString())) {
+            for (Text value : values) {
+                t2Values.add(Integer.parseInt(value.toString()));
             }
         }
-        context.write(key, key.toString().equals("T1") ? new Text(this.t1Values.toString()) : new Text(this.t2Values.toString()));
-        if (key.toString().equals("T2")) {
-            List<Integer> diff = new ArrayList<>(this.t1Values);
-            diff.removeAll(this.t2Values);
-            context.write(new Text(String.valueOf(firstKey) + "-" + key), new Text(diff.toString()));
-            List<Integer> secondDiff = new ArrayList<>(this.t2Values);
-            secondDiff.removeAll(this.t1Values);
-            context.write(new Text(key + "-" + firstKey), new Text(secondDiff.toString()));
-        }
+        t1Values.removeAll(t2Values);
+        context.write(new Text("T1-T2"), new Text(t1Values.toString()));
+
     }
+
 }
